1ccda8c67b298da73a1d99d8e0c14b5f6b5c8ef6
LMGJordao <lmgjordao@gmail.com>
2023-11-24 19:26:10 +0000

    Final delivery
    Put all objects of the scene in the same svg to achieve better responsiveness.

--- js/index.js
+++ js/index.js
@@ -19,33 +19,32 @@ const getScrollProgress = (element, totalElementDepth) => {
     return Math.round(element.scrollTop / totalElementDepth * 100);
 };
 
-const debugScrollProgress = () => {
-    depthIndicator.innerHTML = getScrollProgress(document.documentElement, totalScrollDepth);
-};
-
 // Global variables
 const targetScrollElement = document.documentElement;
 let totalScrollDepth = getTotalScrollDepth(targetScrollElement);
-const depthIndicator = document.getElementById("debug");
 const scene1 = document.getElementById("scene1");
+const row1 = document.getElementById("row1");
+const row2 = document.getElementById("row2");
+const row3 = document.getElementById("row3");
+const row4 = document.getElementById("row4");
+const scene3 = document.getElementById("scene3");
+const scene4 = document.getElementById("scene4");
+
 
 // User interaction
-window.addEventListener("DOMContentLoaded", event => {
-    //Debug information
-    debugScrollProgress();
-})
-
 window.addEventListener("resize", event => {
     //A window resize causes this value to change
     totalScrollDepth = getTotalScrollDepth(document.documentElement);
 });
 
 window.addEventListener("scroll", event => {
-    //Debug information
-    debugScrollProgress();
-
     const depth = getScrollProgress(targetScrollElement, totalScrollDepth);
 
-
     scene1.classList.toggle("hide", !(depth >= 10 && depth <= 30));
+    row1.classList.toggle("hide", !(depth >= 32));
+    row2.classList.toggle("hide", !(depth >= 38));
+    row3.classList.toggle("hide", !(depth >= 44));
+    row4.classList.toggle("hide", !(depth >= 50));
+    scene3.classList.toggle("hide", !(depth >= 58 && depth <= 78));
+    scene4.classList.toggle("hide", !(depth >= 82));
 });
